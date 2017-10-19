a67437f4797f16bf841b4e986adf5753c0b0db4f
jnware7 <jnware7@gmail.com>
2017-10-19 10:23:55 -0700

    Added schema, and project packages

---
+++ src/models/database/schema/schema.sql
@@ -0,0 +1,17 @@
+ CREATE TABLE users(
+   id SERIAL PRIMARY KEY,
+   name VARCHAR(255) UNIQUE NOT NULL,
+   password VARCHAR(255) NOT NULL,
+   email VARCHAR(255)UNIQUE NOT NULL,
+   image VARCHAR(255) DEFAULT 'http://via.placeholder.com/250x250',
+   role VARCHAR(255) DEFAULT user
+ );
+
+ CREATE TABLE quote(
+   id SERIAL PRIMARY KEY,
+   users_id INT REFERENCES users(id),
+   author VARCHAR(255) NOT NULL,
+   quote TEXT,
+   submitted TIMESTAMP DEFAULT now(),
+   likes BOOLEAN default false
+ );
