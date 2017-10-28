3c9ac592cdb2b4a607772716a274ce67a371f1fc
MsMishi <mcwcharles@gmail.com>
2017-10-27 17:45:10 -0700

    Added README.md

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const env = require('dotenv').config();//must be at the top of your project
 const express = require('express');
-const app = require('express')()
+const app = require('express')();
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const routes = require('./controllers/routes');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
-const middlewares = require('./controllers/middlewares')
+const middlewares = require('./controllers/middlewares');
 
 // console.log("env: ", env)
 
@@ -15,36 +15,36 @@ app.set('views', __dirname + '/views');
 
 app.locals.heading = 'Smile';
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 
 app.use(session({
   secret: process.env.SECRET,
   resave:false,
   saveUninitialized: true,
-}))
+}));
 
 app.use((req, res, next) => {
-  req.session.name ='sessionSmile'
-  next()
-})
+  req.session.name ='sessionSmile';
+  next();
+});
 app.use((req, res, next) => {
 
   if(typeof req.session.user === 'object' && Object.keys(req.session.user).length !== 0) {
-    res.locals.isLoggedIn = true
+    res.locals.isLoggedIn = true;
   } else {
-    res.locals.isLoggedIn = false
+    res.locals.isLoggedIn = false;
   }
   console.log('isLoggedIn',res.locals.isLoggedIn);
-  next()
+  next();
 });
 app.use(middlewares.addUserToRequest);
 
-app.use('/', routes)
+app.use('/', routes);
 
-const port = process.env.PORT1 || 3000
+const port = process.env.PORT1 || 3000;
 
 app.listen(port,() => {
-  console.log(`http://localhost:${port}`);
-})
+  console.log(`You're running Smile on http://localhost:${port} =)`);
+});
